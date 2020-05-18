0a6b913f4e2f58ca807c3dbe2ef4289ec0fe37c0
Sidnei Pereira <sidi.jp@gmail.com>
2020-05-17 22:07:05 -0300

    Remove some settings helpers

--- web/src/main/utils/env_parsers.py
+++ web/src/main/utils/env_parsers.py
@@ -1,10 +1,3 @@
-from typing import List
-
-
-def parse_csv_to_list(csv_string: str) -> List[str]:
-    return [value.strip() for value in csv_string.split(',')]
-
-
 def remove_closing_quotes(quoted_string: str) -> str:
     def has_closing_quotes(quoted_string):
         _quotes = ['"', "'"]
@@ -15,7 +8,3 @@ def remove_closing_quotes(quoted_string: str) -> str:
     while has_closing_quotes(quoted_string):
         quoted_string = quoted_string.strip('"').strip("'")
     return quoted_string
-
-
-def parse_bool(boolean_string: str) -> bool:
-    return boolean_string.lower() == 'true'
--- web/tests/main/utils/test_env_parsers.py
+++ web/tests/main/utils/test_env_parsers.py
@@ -6,25 +6,6 @@ from main.utils import env_parsers
 @pytest.mark.unittest
 @pytest.mark.parametrize('a_value, expected', [
     [None, AttributeError],
-    ['', ['']],
-    [',', ['', '']],
-    ['a,b', ['a', 'b']],
-    ['a, b', ['a', 'b']],
-    ['a , b', ['a', 'b']],
-    ['a ,b', ['a', 'b']],
-    [' a , b ', ['a', 'b']],
-])
-def test_parse_csv_to_list(a_value, expected):
-    try:
-        assert env_parsers.parse_csv_to_list(a_value) == expected
-    except BaseException as exception:
-        with pytest.raises(expected):
-            raise exception
-
-
-@pytest.mark.unittest
-@pytest.mark.parametrize('a_value, expected', [
-    [None, AttributeError],
     ['a', 'a'],
     ['"a"', 'a'],
     ["'a'", 'a'],
@@ -37,26 +18,6 @@ def test_parse_csv_to_list(a_value, expected):
 def test_remove_closing_quotes(a_value, expected):
     try:
         assert env_parsers.remove_closing_quotes(a_value) == expected
-    except BaseException as exception:
-        with pytest.raises(expected):
-            raise exception
-
-
-@pytest.mark.unittest
-@pytest.mark.parametrize('a_value, expected', [
-    [None, AttributeError],
-    ['true', True],
-    ['True', True],
-    ["TRUE", True],
-    ['', False],
-    ['false', False],
-    ['False', False],
-    ['FALSE', False],
-    ['some string', False],
-])
-def test_parse_bool(a_value, expected):
-    try:
-        assert env_parsers.parse_bool(a_value) == expected
     except BaseException as exception:
         with pytest.raises(expected):
             raise exception
